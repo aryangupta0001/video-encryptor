49dc90b1393c8e39f5739f28357bea10839b82a1
Aryan Gupta <aryanguptabareilly067@gmail.com>
2024-09-25 03:12:17 +0530

    feat : added video  file type check & drop video functionality

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,11 +6,39 @@ import { useState } from 'react'
 const Home = () => {
   const [btnName, setBtnName] = useState("Browse");
   const [file, setFile] = useState(null);
+  const [dragging, setDragging] = useState(false);
 
 
   const handleNewFile = (e) => {
     console.log("Browse Button Clicked");
     setFile(e.target.files[0]);
+  }
+
+  const handleFileDragOver = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  }
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  }
+
+  const handleFileDrop = (e) => {
+    e.preventDefault();
+
+    console.log("File Dropped");
+
+    if(e.dataTransfer.files)
+        if(e.dataTransfer.files.length > 0)
+        { const newFile = e.dataTransfer.files[0];
+
+          if(newFile.type.startsWith("video/"))
+            setFile(newFile);
+
+          else
+            alert("Video File Not Found");
+        }
+
   }
 
   return (
@@ -21,7 +49,7 @@ const Home = () => {
         </h1>
       </center>
 
-      <div id="dragDrop">
+      <div id="dragDrop" onDragOver={handleFileDragOver} onDragLeave={handleDragLeave} onDrop={handleFileDrop}>
         {/* <div> */}
         <img src={uploadImg} alt="" />
         {/* </div> */}
@@ -29,14 +57,14 @@ const Home = () => {
         <br></br>
         <br></br>
         OR
-        <input type="file" id='browseFileBtn' style={{ display: 'none' }} onChange={handleNewFile} />
+        <input type="file" accept="video/*" id='browseFileBtn' style={{ display: 'none' }} onChange={handleNewFile} />
 
         <label htmlFor="browseFileBtn" className='button' >{file ? file.name : "Browse File"}</label>
 
-      <div>
-      </div>
+        <div>
+        </div>
 
-    </div>
+      </div>
 
     </div >
   )
